5fe8c9fd3e9ab57c3228d209bee893fea2244af7
Prakhar Mathur <prakharmathur619@gmail.com>
2021-03-18 13:14:20 +0530

    changed orders query

--- src/queries/orders.ts
+++ src/queries/orders.ts
@@ -37,7 +37,6 @@ export const ordersByUser = gql`
                   name
                   id
                 }
-                quantityOrdered
               }
               thumbnail {
                 alt
