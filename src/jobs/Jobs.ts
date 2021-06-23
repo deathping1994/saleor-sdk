3a7c71507b2f130cf02c0426d2e9592d296597dd
mathur619 <prakharmathur619@gmail.com>
2021-06-23 16:30:02 +0530

    added wallet api

--- src/jobs/Jobs.ts
+++ src/jobs/Jobs.ts
@@ -2,12 +2,14 @@ import { ApolloClientManager } from "../data/ApolloClientManager";
 import { LocalStorageHandler } from "../helpers/LocalStorageHandler";
 import { AuthJobs } from "./Auth";
 import { CheckoutJobs } from "./Checkout";
+import { WalletJobs } from "./Wallet";
 import { WishlistJobs } from "./Wishlist";
 
 export interface IJobs {
   auth: AuthJobs;
   checkout: CheckoutJobs;
   wishlist: WishlistJobs;
+  wallet: WalletJobs;
 }
 
 export class Jobs implements IJobs {
@@ -17,6 +19,8 @@ export class Jobs implements IJobs {
 
   wishlist: WishlistJobs;
 
+  wallet: WalletJobs;
+
   constructor(
     localStorageHandler: LocalStorageHandler,
     apolloClientManager: ApolloClientManager
@@ -24,5 +28,6 @@ export class Jobs implements IJobs {
     this.auth = new AuthJobs(localStorageHandler, apolloClientManager);
     this.checkout = new CheckoutJobs(localStorageHandler, apolloClientManager);
     this.wishlist = new WishlistJobs(localStorageHandler, apolloClientManager);
+    this.wallet = new WalletJobs(localStorageHandler, apolloClientManager);
   }
 }
