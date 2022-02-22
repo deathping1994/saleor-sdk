91935784952ae54d06756e44fbf4c6cbbc7015eb
Koustav <koustavlmcr17@gmail.com>
2022-02-22 19:46:20 +0530

    added missing usermetaaccess

--- src/queries/gqlTypes/UserMetaDetails.ts
+++ src/queries/gqlTypes/UserMetaDetails.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { UserMetaAccess } from "./../gqlTypes/globalTypes";
+import { UserMetaAccess } from "./../../gqlTypes/globalTypes";
 
 // ====================================================
 // GraphQL query operation: UserMetaDetails
