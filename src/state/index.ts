6c2db48c414d06c8f6b9fae6d500000db677fd7f
Hitesh <hdk.hitesh26@gmail.com>
2022-02-23 12:18:53 +0530

    added tax in totalPrice

--- src/state/index.ts
+++ src/state/index.ts
@@ -364,7 +364,8 @@ export class SaleorState extends NamedObservable<StateItems> {
 
     if (items && items.length && items[0].quantity > 0) {
       // console.log(checkout?.token);
-
+      const checkoutSubtotal = checkout?.subtotalPrice;
+      const checkoutTotal = checkout?.totalPrice;
       const { data, error } =
         checkout?.token &&
         (await this.getCouponPrepaidDiscount(checkout?.token));
@@ -468,6 +469,7 @@ export class SaleorState extends NamedObservable<StateItems> {
               2
             ),
           },
+          tax: checkoutSubtotal?.tax,
         };
 
         const totalPrice = {
@@ -486,6 +488,7 @@ export class SaleorState extends NamedObservable<StateItems> {
               2
             ),
           },
+          tax: checkoutTotal?.tax,
         };
 
         const mrp = {
